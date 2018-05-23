aacf4bdb12dcae0708943aad741fb2094351c72b
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2018-05-22 21:11:15 -0400

    Use heap-sort instead.

--- merge_k_sorted_lists.py
+++ merge_k_sorted_lists.py
@@ -3,6 +3,8 @@ Merge k sorted linked lists and return it as one sorted list. Analyze and
 describe its complexity.
 """
 
+from queue import PriorityQueue
+
 
 # Definition for singly-linked list.
 class ListNode:
@@ -17,42 +19,25 @@ class Solution:
         :type lists: List[ListNode]
         :rtype: ListNode
         """
-        if len(lists) is 0:
-            return None
+        min_heap = PriorityQueue()
 
-        head = None
-
+        # Heap sort
         for l in lists:
+            node = l
+            while node is not None:
+                min_heap.put(node.val)
+                node = node.next
+
+        head, curr = None, None
+
+        while not min_heap.empty():
+            node = ListNode(min_heap.get())
+
             if head is None:
-                head = l
+                head = node
+                curr = node
             else:
-                head = self.merge(head, l)
+                curr.next = node
+                curr = node
 
         return head
-
-    def merge(self, list1, list2):
-        """
-        :type list1: ListNode
-        :type list2: ListNode
-        :rtype: ListNode
-        """
-        curr_node, head_node = None, None
-
-        while 1:
-            if list1 is None and list2 is None:
-                break
-            elif list1 is not None and (list2 is None or list1.val <= list2.val):
-                next_node = ListNode(list1.val)
-                list1 = list1.next
-            else:
-                next_node = ListNode(list2.val)
-                list2 = list2.next
-
-            if curr_node is not None:
-                curr_node.next = next_node
-            else:
-                head_node = next_node
-
-            curr_node = next_node
-
-        return head_node
